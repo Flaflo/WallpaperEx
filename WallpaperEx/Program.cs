0841328b5ebdf50d300a773fa4a4638274687334
Lukas Klinger <flaflo@ok.de>
2022-02-10 16:32:42 +0100

    :art: Using proper modifiers

--- WallpaperEx/Program.cs
+++ WallpaperEx/Program.cs
@@ -2,13 +2,13 @@ using WallpaperEx.Forms;
 
 namespace WallpaperEx;
 
-static class Program
+internal static class Program
 {
     /// <summary>
     ///  The main entry point for the application.
     /// </summary>
     [STAThread]
-    static void Main()
+    private static void Main()
     {
         // To customize application configuration such as set high DPI settings or default font,
         // see https://aka.ms/applicationconfiguration.
